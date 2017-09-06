51af43315d6b555076324ead83d37f71de2ed587
o7 <olegmarko7@gmail.com>
2017-09-06 17:58:46 +0300

    add Sign Up method

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -2,13 +2,13 @@
 	<div>
 		<h3 class="text-center">Register</h3>
 
-		<input type="email" class="form-control m-b-15" placeholder="Email address">
-		<input type="text" class="form-control m-b-15" placeholder="Username">
-		<input type="password" class="form-control m-b-15" placeholder="Password">
+		<input v-model="user.email" type="email" class="form-control m-b-15" placeholder="Email address">
+		<input v-model="user.username" type="text" class="form-control m-b-15" placeholder="Username">
+		<input v-model="user.password" type="password" class="form-control m-b-15" placeholder="Password">
 
 		<hr>
 
-		<button class="btn btn-lg btn-primary btn-block m-b-15">Register</button>
+		<button @click="register" class="btn btn-lg btn-primary btn-block m-b-15">Register</button>
 
 		<p class="text-center">
 			Already have an account? 
@@ -19,7 +19,34 @@
 
 <script>
 	export default {
-		name: "register"
+		name: "register",
+		data: function() {
+			return {
+				user: {
+					email: '',
+					username: '',
+					password: ''
+				}
+			}
+		},
+		methods: {
+			register: function() {
+				this.$http.post(process.env.API_URL + '/users', this.user)
+					.then(function (response) {
+						alertify.success(
+							"Success! You can now login with your email and password."
+						)
+						this.$router.push('/auth/login')
+					})
+					.catch(function (response) {
+						if (response.status == 422) {
+							response.body.errors.forEach((e) => {
+								alertify.error(e)
+							})
+						}
+					})
+			}
+		}
 	}
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
 
 require('../node_modules/bootstrap/less/bootstrap.less')
 require('./assets/beeper.css')
+require('./assets/alertify/alertify.min.css')
+require('./assets/alertify/alertify.min.js')
+
+alertify.defaults.notifier.position = 'top-right'
 
 Vue.config.productionTip = false
+
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
